52073ed03b4a75f74e587aaa7ca68977fa4bfab2
CarlosMBarboza <techosunion@hotmail.com>
2023-11-17 10:06:40 -0300

    fin

--- src/Pages/ContactanosPage.jsx
+++ src/Pages/ContactanosPage.jsx
@@ -20,46 +20,44 @@ const ContactanosPage = () => {
         alert("Todos los campos son obligatorios")
         return;
         }
-     
-  }
-  
-  return (
-    <div className="form-contacto" >
+}
+        return (
+            <div className="form-contacto" >
 
-    <form  className="form-input" onSubmit={handleSubmit} ref={form}>
-      <input
-        className="form-control mb-2" 
-        type="text"
-        name="name"
-        placeholder="Nombre y Apellido" 
-        defaultValue='name'
-        />
+        <form  className="form-input" onSubmit={handleSubmit} ref={form}>
         <input
-        className="form-control mb-2" 
-        type="tel"
-        name="phone"
-        placeholder="Ingrese su telefono" 
-        defaultValue='phone'
-        />
-        <input
-        className="form-control mb-2" 
-        type="email"
-        name="email"
-        placeholder="Ingrese su Email" 
-        defaultValue='email'
-        />
-        <textarea
-        className="form-control mb-3" 
-        placeholder="Ingrese Descripcion"
-        name="description"
-        defaultValue='description'
-        />
-        <select  className="form-select mb-2" name="state"  defaultValue='completado'>
-            <option value="pendiente">Pedido Pendiente</option>
-            <option value="completado">Pedido Completado</option>
-        </select>
-            <button className="btn btn-info">Enviar</button>
-        </form>
+            className="form-control mb-2" 
+            type="text"
+            name="name"
+            placeholder="Nombre y Apellido" 
+            defaultValue='name'
+            />
+            <input
+                className="form-control mb-2" 
+                type="tel"
+                name="phone"
+                placeholder="Ingrese su telefono" 
+                defaultValue='phone'
+                />
+                <input
+                className="form-control mb-2" 
+                type="email"
+                name="email"
+                placeholder="Ingrese su Email" 
+                defaultValue='email'
+                />
+                <textarea
+                className="form-control mb-3" 
+                placeholder="Ingrese Descripcion"
+                name="description"
+                defaultValue='description'
+                />
+                <select  className="form-select mb-2" name="state"  defaultValue='completado'>
+                    <option value="pendiente">Pedido Pendiente</option>
+                    <option value="completado">Pedido Completado</option>
+                </select>
+                    <button className="btn btn-info">Enviar</button>
+                </form>
 
     </div>
     )
